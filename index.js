c78e68c430a64d381019dd8b65ba627a8bd74575
AkankshaNangare <akankshanangare5055@gmail.com>
2024-10-28 10:33:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// variables
+
+var age=25;
+
+if(true){
+    console.log(age);
+}
+
+{
+    let a=10;
+    console.log(a);
+}
+
+const b=20;
+console.log(b)
---
+++ operators.js
@@ -0,0 +1,46 @@
+// ARITHMETIC OPERATORS
+
+let a=2;
+let b=5;
+
+// console.log(a*b);
+
+a=a+2;
+console.log(a);
+
+b -=2;
+console.log(b);
+
+// COMPARISON OPERATORS
+
+console.log(50>=5);
+console.log(50<=5);
+
+console.log('5' ==5);
+console.log('5'===5);
+
+// TERNARY OPERATORS
+
+ let age=25;
+ let status=(age>18)?'I can vote' : 'I cannot vote'
+ console.log(status)
+
+//  logical operators
+
+let ans=(true && true &&  true)
+console.log(ans);
+
+let ans1=(true && false &&  true)
+console.log(ans1);
+
+let ans2=(false || false ||  false)
+console.log(ans2);
+
+let ans3= !(false)
+console.log(ans3);
+
+// bitwise operators
+console.log(2&5);
+
+console.log(2|5);
+
