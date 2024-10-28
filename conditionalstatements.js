3d49a934d9d2a7650f79d869a99edbfdd97168b6
AkankshaNangare <akankshanangare5055@gmail.com>
2024-10-28 13:44:19 +0530

    conditional statements

---
+++ conditionalstatements.js
@@ -0,0 +1,52 @@
+
+// IF__ElSE STATEMENT
+
+let number=5;
+
+if(number==1){
+    console.log('A');
+
+}
+else{
+    console.log('no')
+}
+
+// ELSE-IF STATEMENT
+
+let number1=1;
+
+if(number1==1){
+    console.log('A');
+
+}
+else if (number1==2){
+    console.log('B');
+}
+else if (number1==3){
+    console.log('C');
+}
+else if (number1==4){
+    console.log('D');
+}
+else if (number1==5){
+    console.log('E');
+}
+else
+{
+    console.log('F');
+}
+
+// SWITCH CASE STATEMENT
+
+let num = 3;
+switch(num){
+    case 1 : console.log('A');
+    break;
+    case 2 : console.log('B');
+    break;
+    case 3 : console.log('C');
+    break;
+    case 4 : console.log('D');
+    break;
+    default : console.log('F');
+}
