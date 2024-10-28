bb1e9c85e4d7c6308defe5d42e63b0490e3e125b
AkankshaNangare <akankshanangare5055@gmail.com>
2024-10-28 13:43:58 +0530

    loops statements

---
+++ loops.js
@@ -0,0 +1,61 @@
+
+// FOR LOOP
+
+for(let i=1;i<=10 ;i++){
+console.log("akanksha")
+
+}
+
+for(let i=1;i<=5 ;i++){
+    console.log(i);
+}
+
+for(let i=5;i>0;i--){
+    console.log(i);
+}
+
+
+for(let i=1;i<=6;i++){
+    if(i==4){
+        break;
+    }
+    else{
+    console.log(i);
+    }
+}
+
+for(let i=1;i<=4;i++){
+    if(i==3){
+        continue;
+    }
+    else{
+    console.log(i);
+    }
+}
+
+
+// WHITE LOOP
+
+let i=1;
+while(i<=10){
+    console.log("AAAA");
+    i++;
+}
+
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// DO_WHILE LOOP
+
+
+let j=1;
+do{
+    console.log("akas");
+    j++;
+}while(j<=10);
