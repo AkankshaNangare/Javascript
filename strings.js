b6e5c0ca82f0751f3e84be7a9396456feb97bc46
AkankshaNangare <akankshanangare5055@gmail.com>
2024-10-29 14:16:03 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,21 @@
+
+let firstname="ak";
+let lastname="akanksha";
+
+let name=`akanksha
+    this is my
+    name`;
+
+console.log(name);
+
+let op1='english';
+let op2='hindi';
+
+// console.log(`${op1}${op2}`);
+
+console.log(op1.length);
+
+console.log(op2.toUpperCase())
+
+let str="akanksha";
+console.log(str.substring(2));
