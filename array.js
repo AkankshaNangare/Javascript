eb98bd8ebc98bc01cd634f96b41dbaeac2272103
AkankshaNangare <akankshanangare5055@gmail.com>
2024-10-30 12:19:33 +0530

    arrays

--- array.js
+++ array.js
@@ -72,6 +72,39 @@ console.log(arr.indexOf(50));
 let length=arr.length;
 console.log("length : ",length);
 
-arr.forEach((value,index)=>{
-    console.log("Number : " ,value, "Index :",index)
-})
+// TRADITIONAL LOOP
+for (let index = 0; index < length; index++) {
+    console.log(arr[index]);
+    
+}
+
+
+
+// arr.forEach((value,index)=>{
+//     console.log("Number : " ,value, "Index :",index)
+// })
+
+
+for (let key in obj) {
+    console.log(key);
+}
+
+let fullName="abcdef";
+
+for (let val of fullName) {
+    console.log(val)
+}
+
+// ARRAY WITH FUNCTIONS
+
+let arr1=[10,20,30,40,50]
+
+function getSum(arr1){
+    let sum=0;
+    for (let index = 0; index < arr1.length; index++) {
+        sum = sum + arr1[index ] ; 
+    }
+    return sum
+}
+let totalSum = getSum(arr1);
+console.log(totalSum);
