59a29c25ceeaff3aadbc205cc254a418b12e6c71
AkankshaNangare <akankshanangare5055@gmail.com>
2024-10-30 12:19:52 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,23 @@
+
+// VARIABLE AND FUNCTION HOISTING
+
+let age=25;
+console.log(age);
+
+const a=25;
+console.log(a);
+
+
+
+sayMyName("akanksha");
+
+function sayMyName(firstName){
+    console.log(firstName);
+}
+
+
+class Human{
+
+}
+const obj1 = new Human();
+
